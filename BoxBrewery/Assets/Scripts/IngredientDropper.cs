3234e90c3c47bc3841f5fc6b11c4d7601d648126
harold fu <waterdude551@gmail.com>
2026-03-07 13:28:56 -0500

    drop ingredient animation

--- BoxBrewery/Assets/Scripts/IngredientDropper.cs
+++ BoxBrewery/Assets/Scripts/IngredientDropper.cs
@@ -1,14 +1,18 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class IngredientDropper : MonoBehaviour
 {
     [SerializeField]
-    private DroppedIngredient[] _droppedIngredientsPool;
+    private List<DroppedIngredient> _droppedIngredientsPool = new List<DroppedIngredient>();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _droppedIngredientsPool = GetComponentsInChildren<DroppedIngredient>();
+        foreach (DroppedIngredient ingredient in GetComponentsInChildren<DroppedIngredient>()) {
+            _droppedIngredientsPool.Add(ingredient);
+            ingredient.gameObject.SetActive(false);
+        }
     }
 
     public void DropIngredient(int ingredientIndex)
@@ -17,6 +21,7 @@ public class IngredientDropper : MonoBehaviour
         {
             if (!ingredient.gameObject.activeInHierarchy)
             {
+                ingredient.gameObject.SetActive(true);
                 ingredient.Activate(ingredientIndex);
                 break;
             }
