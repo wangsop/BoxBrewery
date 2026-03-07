5d834f67d6f9209ed356505102601731b1ec4ba6
harold fu <waterdude551@gmail.com>
2026-03-07 14:52:38 -0500

    Cauldron list and bugfix
    List next to cauldron of current ingredients.
    Removed brewing slop with nothing. nothingslop.

---
+++ BoxBrewery/Assets/Scripts/CauldronLister.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CauldronLister : MonoBehaviour
+{
+    private TextMeshProUGUI _tmp;
+    [SerializeField]
+    private IngredientShelf _shelf;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _tmp = GetComponent<TextMeshProUGUI>();
+        if (_shelf == null)
+            _shelf = FindFirstObjectByType<IngredientShelf>();
+
+    }
+
+    private string BuildString()
+    {
+        Dictionary<string, int> dict = _shelf.HoldingCellToDict();
+        string s = "";
+        foreach (KeyValuePair<string, int> kvp in dict)
+        {
+            string line = string.Format("{0}x {1}\n", kvp.Value, kvp.Key);
+            s += line;
+        }
+        return s;
+    }
+
+    public void UpdateText()
+    {
+        string s = BuildString();
+        _tmp.text = s;
+    }
+}
