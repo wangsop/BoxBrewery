2d48f64c5733ffb66de37de2d7d313ca48ad6f8c
wangsop <sophia.riverfront@gmail.com>
2026-03-07 21:36:56 -0500

    Reanimate, sprite migration
    Adding sprites for title screen, added how to play button, added sprites for order scene, reanimated customer

--- BoxBrewery/Assets/Scripts/SceneManager.cs
+++ BoxBrewery/Assets/Scripts/SceneManager.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class SceneManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-
+    [SerializeField] private GameObject popup;
     // Update is called once per frame
     void Update()
     {
@@ -17,5 +17,9 @@ public class SceneManager : MonoBehaviour
     public void QuitToDesktop()
     {
         Application.Quit();
+    }
+    public void Popup()
+    {
+        popup.SetActive(!popup.activeSelf);
     }
 }
